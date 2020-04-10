1bfdbb91aa06434114d3486f824d22e34997d7d8
farid <jefferson.farid@hotmai.com>
2020-04-10 11:27:05 -0500

    clase 72 router events  10/04/2020

---
+++ src/app/title.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subject, Observable } from 'rxjs';
+import { tap, map, filter } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class TitleService {
+    private titleSubject: Subject<string> = new Subject();
+    public title$: Observable<string> = this.titleSubject.asObservable();
+    constructor(
+        private router: Router
+    ) {
+        this.init();
+    }
+
+    init() {
+        this.router.events.pipe(
+            filter(event => event instanceof NavigationEnd),
+            map((evt: NavigationEnd) => evt.urlAfterRedirects)
+        ).subscribe(title => this.titleSubject.next(title));
+    }
+}
