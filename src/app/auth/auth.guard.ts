44fe5ae6f24ca85146282eaa8cf86171fc793656
farid <jefferson.farid@hotmai.com>
2020-04-18 14:37:33 -0500

    clase 82 canLoad  18/04/2020

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, CanLoad, ActivatedRouteSnapshot, RouterStateSnapshot, Router, Route, UrlSegment } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable({
     providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
     constructor(private authService: AuthService, private router: Router) { }
+
+    canLoad(route: Route, segments: UrlSegment[]): boolean {
+        const url = segments.map(item => item.path).join('/');
+        return this.checkLogin(url);
+    }
 
     canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
         return this.checkLogin(state.url);
